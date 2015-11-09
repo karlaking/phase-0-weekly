6327814c44dcb61a44199bad69f206b12d664c82
Karla King <karlaking2@.com>
2015-11-09 13:36:59 -0800

    Add reflection to guessing-game

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -101,10 +101,13 @@ p game.solved?   == true
 # Reflection
 =begin
 How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
-	
+	The capabilities and attributes of an instance variable is defined by the Class it is in. 
 When should you use instance variables? What do they do for you?
+  Instance variables may be passed from one method to the next within a Class. Information can be computed and passed on to the next method easily for more complex programs. They are designated using an @ symbol.
 Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
-Why do you think this code requires you to return symbols? What are the benefits of using symbols?	
+  Flow control in ruby looks like an if, elsif, and else statement. It allows us to sort information, based on circumstances. This is one of the great strengths of programming. The challenge was relatively simple to sort using the if statements. What was more interesting was practicing with instance variables.
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+  Symbols are more easily stored and efficient. 	
 
 	
 =end
